da3d47a546fd258f803672896d34908514a9ecb3
Leonardo Costa <leonardo.costa.santos.si@gmail.com>
2023-09-01 11:37:18 -0300

    22.14. Configurando a validade e não reutilização de Refresh Tokens

--- src/main/java/com/algaworks/algafood/auth/AuthorizationServerConfig.java
+++ src/main/java/com/algaworks/algafood/auth/AuthorizationServerConfig.java
@@ -3,6 +3,7 @@ package com.algaworks.algafood.auth;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
@@ -12,32 +13,34 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Aut
 
 @Configuration
 @EnableAuthorizationServer
-public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter{
+public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
 
 	@Autowired
 	private PasswordEncoder passwordEncoder;
-	
+
 	@Autowired
 	private AuthenticationManager authenticationManager;
-	
+
+	@Autowired
+	private UserDetailsService userDetailsService;
+
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-		clients.inMemory()
-				.withClient("algafood-web")
-					.secret(passwordEncoder.encode("web123"))
-					.authorizedGrantTypes("password")
-					.scopes("write", "read")
-					.accessTokenValiditySeconds(60 * 60 * 6);
-			
+		clients.inMemory().withClient("algafood-web").secret(passwordEncoder.encode("web123"))
+				.authorizedGrantTypes("password", "refresh_token").scopes("write", "read")
+				.accessTokenValiditySeconds(6 * 60 * 60) // 6 horas
+				.refreshTokenValiditySeconds(60 * 24 * 60 * 60); // 60 dias
+
 	}
-	
+
 	@Override
 	public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
-		security.checkTokenAccess("isAuthenticated()");
+//		security.checkTokenAccess("isAuthenticated()");
+		security.checkTokenAccess("permitAll()");
 	}
-	
+
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-		endpoints.authenticationManager(authenticationManager);
+		endpoints.authenticationManager(authenticationManager).userDetailsService(userDetailsService).reuseRefreshTokens(false);
 	}
 }
