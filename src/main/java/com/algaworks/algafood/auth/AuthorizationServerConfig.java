97926a4da3b26e8b431884297dc0ea3cd3ad6bd8
Leonardo Costa <leonardo.costa.santos.si@gmail.com>
2023-09-02 17:07:07 -0300

    Implementando o suporte a PKCE com fluxo Authorization Code

--- src/main/java/com/algaworks/algafood/auth/AuthorizationServerConfig.java
+++ src/main/java/com/algaworks/algafood/auth/AuthorizationServerConfig.java
@@ -1,5 +1,7 @@
 package com.algaworks.algafood.auth;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -10,6 +12,8 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.CompositeTokenGranter;
+import org.springframework.security.oauth2.provider.TokenGranter;
 
 @Configuration
 @EnableAuthorizationServer
@@ -26,36 +30,22 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-		clients.inMemory()
-					.withClient("algafood-web")
-					.secret(passwordEncoder.encode("web123"))
-					.authorizedGrantTypes("password", "refresh_token")
-					.scopes("write", "read")
-					.accessTokenValiditySeconds(6 * 60 * 60) // 6 horas
-					.refreshTokenValiditySeconds(60 * 24 * 60 * 60) // 60 dias
-					
-				.and()
-					.withClient("foodanalytics")
-					.secret(passwordEncoder.encode("food123"))
-					.authorizedGrantTypes("authorization_code")
-					.scopes("write", "read")
-					.redirectUris("http://localhost:8082")
-					
-				.and()
-					.withClient("webadmin")
-					.authorizedGrantTypes("implicit")
-					.scopes("write", "read")
-					.redirectUris("http://aplicacao-cliente")
-				
-				.and()
-					.withClient("faturamento")
-					.secret(passwordEncoder.encode("faturamento123"))
-					.authorizedGrantTypes("client_credentials")
-					.scopes("write", "read")
-					
-				.and()
-					.withClient("checktoken")
-					.secret(passwordEncoder.encode("check123"));
+		clients.inMemory().withClient("algafood-web").secret(passwordEncoder.encode("web123"))
+				.authorizedGrantTypes("password", "refresh_token").scopes("write", "read")
+				.accessTokenValiditySeconds(6 * 60 * 60) // 6 horas
+				.refreshTokenValiditySeconds(60 * 24 * 60 * 60) // 60 dias
+
+				.and().withClient("foodanalytics").secret(passwordEncoder.encode("food123"))
+				.authorizedGrantTypes("authorization_code").scopes("write", "read")
+				.redirectUris("http://localhost:8082")
+
+				.and().withClient("webadmin").authorizedGrantTypes("implicit").scopes("write", "read")
+				.redirectUris("http://aplicacao-cliente")
+
+				.and().withClient("faturamento").secret(passwordEncoder.encode("faturamento123"))
+				.authorizedGrantTypes("client_credentials").scopes("write", "read")
+
+				.and().withClient("checktoken").secret(passwordEncoder.encode("check123"));
 	}
 
 	@Override
@@ -66,6 +56,17 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-		endpoints.authenticationManager(authenticationManager).userDetailsService(userDetailsService).reuseRefreshTokens(false);
+		endpoints.authenticationManager(authenticationManager).userDetailsService(userDetailsService)
+				.reuseRefreshTokens(false).tokenGranter(tokenGranter(endpoints));
+	}
+
+	private TokenGranter tokenGranter(AuthorizationServerEndpointsConfigurer endpoints) {
+		var pkceAuthorizationCodeTokenGranter = new PkceAuthorizationCodeTokenGranter(endpoints.getTokenServices(),
+				endpoints.getAuthorizationCodeServices(), endpoints.getClientDetailsService(),
+				endpoints.getOAuth2RequestFactory());
+
+		var granters = Arrays.asList(pkceAuthorizationCodeTokenGranter, endpoints.getTokenGranter());
+
+		return new CompositeTokenGranter(granters);
 	}
 }
