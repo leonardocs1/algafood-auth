27836ea47697b4a990b27ab8f6b402cf7e73330b
Leonardo Costa <leonardo.costa.santos.si@gmail.com>
2023-09-05 20:05:12 -0300

    23.9. Assinando o JWT com RSA SHA-256 (chave assimétrica)

--- src/main/java/com/algaworks/algafood/auth/AuthorizationServerConfig.java
+++ src/main/java/com/algaworks/algafood/auth/AuthorizationServerConfig.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -16,6 +17,7 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Aut
 import org.springframework.security.oauth2.provider.CompositeTokenGranter;
 import org.springframework.security.oauth2.provider.TokenGranter;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
 
 @Configuration
 @EnableAuthorizationServer
@@ -32,38 +34,30 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-		clients.inMemory().withClient("algafood-web")
-						  .secret(passwordEncoder.encode("web123"))
-						  .authorizedGrantTypes("password", "refresh_token")
-						  .scopes("write", "read")
-						  .accessTokenValiditySeconds(6 * 60 * 60) // 6 horas
-						  .refreshTokenValiditySeconds(60 * 24 * 60 * 60) // 60 dias
+		clients.inMemory().withClient("algafood-web").secret(passwordEncoder.encode("web123"))
+				.authorizedGrantTypes("password", "refresh_token").scopes("write", "read")
+				.accessTokenValiditySeconds(6 * 60 * 60) // 6 horas
+				.refreshTokenValiditySeconds(60 * 24 * 60 * 60) // 60 dias
 
 				.and()
-				
-					     .withClient("foodanalytics").secret(passwordEncoder.encode("food123"))
-					     .authorizedGrantTypes("authorization_code")
-					     .scopes("write", "read")
-					     .redirectUris("http://localhost:8082")
+
+				.withClient("foodanalytics").secret(passwordEncoder.encode("food123"))
+				.authorizedGrantTypes("authorization_code").scopes("write", "read")
+				.redirectUris("http://localhost:8082")
 
 				.and()
-				
-						.withClient("webadmin")
-						.authorizedGrantTypes("implicit")
-						.scopes("write", "read")
-						.redirectUris("http://aplicacao-cliente")
+
+				.withClient("webadmin").authorizedGrantTypes("implicit").scopes("write", "read")
+				.redirectUris("http://aplicacao-cliente")
 
 				.and()
-				
-						.withClient("faturamento")
-						.secret(passwordEncoder.encode("faturamento123"))
-						.authorizedGrantTypes("client_credentials", "refresk_token")
-						.scopes("write", "read")
+
+				.withClient("faturamento").secret(passwordEncoder.encode("faturamento123"))
+				.authorizedGrantTypes("client_credentials", "refresk_token").scopes("write", "read")
 
 				.and()
-				
-						.withClient("checktoken")
-						.secret(passwordEncoder.encode("check123"));
+
+				.withClient("checktoken").secret(passwordEncoder.encode("check123"));
 	}
 
 	@Override
@@ -81,8 +75,16 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 
 	@Bean
 	public JwtAccessTokenConverter jwtAccessTokenConverter() {
-		JwtAccessTokenConverter jwtAccessTokenConverter = new JwtAccessTokenConverter();
-		jwtAccessTokenConverter.setSigningKey("l2kj3lkj2l3j2lkj3lk2j3lkj2l3j2lj3lk2j3lj3l232lj3l2j3qqq");
+		var jwtAccessTokenConverter = new JwtAccessTokenConverter();
+
+		var jksResource = new ClassPathResource("keystores/algafood.jks");
+		var keyStorePass = "123456";
+		var keyPairAlias = "algafood";
+
+		var keyStoreKeyFactory = new KeyStoreKeyFactory(jksResource, keyStorePass.toCharArray());
+		var keyPair = keyStoreKeyFactory.getKeyPair(keyPairAlias);
+		
+		jwtAccessTokenConverter.setKeyPair(keyPair);
 
 		return jwtAccessTokenConverter;
 	}
